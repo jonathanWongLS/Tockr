1c133a7f1c023cc51fe705f2030af32b6f4ddd89
JonWLS <75321066+jonathanWongLS@users.noreply.github.com>
2024-07-03 23:59:23 +0800

    Add command handlers for creating projects, tasks, fetching details, listing projects, and deleting tasks

--- src/index.js
+++ src/index.js
@@ -29,6 +29,310 @@ client.on('ready', async (c) => {
       console.log(`Logged in as ${client.user.tag}!`);
 });
 
+client.on('interactionCreate', async (interaction) => {
+      if (!interaction.isChatInputCommand()) return;
+
+      if (interaction.commandName === 'create-project') {
+            const projectName = await getUniqueProjectName(interaction.options.getString("project-name"));
+            const projectOwnerID = interaction.user.id;
+            var projectPassword = interaction.options.getString("project-password");
+            const projectDescription = interaction.options.getString("project-description");
+
+            // Assign a unique project ID
+            var newId = uuidv4();
+            var projectRef = DB.collection('projects').doc(newId);
+            var doc = await projectRef.get();
+            while (doc.exists) {
+                  newId = uuidv4();
+                  projectRef = DB.collection('projects').doc(newId);
+                  doc = await projectRef.get();
+            }
+            const projectId = newId;
+
+            // Store encrypted version of password 
+            if (projectPassword) {
+                  projectPassword = await encryptPassword(projectPassword);
+            } else {
+                  projectPassword = null;
+            }
+
+            // Add the project to the database
+            DB.collection("projects").doc(projectId).set(
+                  {
+                        projectId: projectId,
+                        projectName: projectName,
+                        projectOwner: projectOwnerID,
+                        projectPassword: projectPassword,
+                        projectDescription: projectDescription,
+                  }
+            )
+            interaction.reply(`Project with name **${projectName}** has been created! \nCreated by <@${interaction.user.id}>`);
+            return;
+      }
+
+      if (interaction.commandName === 'create-task') {
+            const projectName = interaction.options.getString('project-name');
+            const projectPassword = interaction.options.getString('project-password');
+            const taskName = interaction.options.getString('task-name');
+            const taskDescription = interaction.options.getString('task-description') || "";
+            const taskDeadline = interaction.options.getString('task-deadline');
+            const assignedUsers = {
+                  user1: null,
+                  user2: null,
+                  user3: null,
+                  user4: null,
+                  user5: null,
+            }
+            const password = interaction.options.getString('project-password');
+
+            for (let i = 0; i <= 5; i++) {
+                  const userData = interaction.options.getUser(`assigned-user-${i}`);
+                  if (userData) {
+                        assignedUsers[`user${i}`] = userData.id;
+                  }
+            }
+
+            // Validate project name
+            const projectsRef = DB.collection('projects');
+            const snapshot = await projectsRef.where('projectName', '==', projectName).get();
+            if (snapshot.empty) {
+                  interaction.reply(`Project name '**${projectName}**' does not exist!\nCreate a **new project**, or find for a valid project name with */list-project-names*. \n <@${interaction.user.id}>`);
+                  return;
+            }
+
+            const projectData = snapshot.docs[0].data();
+
+            if (projectData.projectPassword) {
+                  if (!projectPassword) {
+                        interaction.reply(`Project \`${projectData.projectName}\` is protected with a password. Enter the correct password. \n <@${interaction.user.id}>`);
+                        return;
+                  } 
+                  const correctPassword = await passwordIsCorrect(projectPassword, projectData.projectPassword);
+                  
+                  if (!correctPassword) {
+                        interaction.reply(`Incorrect password for project \`${projectData.projectName}\`. Enter the __correct password__. \n <@${interaction.user.id}>`);
+                        return;
+                  }
+            }
+
+            // Validate task deadline format is DD/MM/YYYY HH:MM:SS
+            if (!isValidDateTimeString(taskDeadline)) {
+                  interaction.reply(`Invalid deadline format. Please use **DD/MM/YYYY HH:MM:SS** \n <@${interaction.user.id}>`);
+                  return;
+            }
+
+            // Save task to database (Firestore)
+            DB.collection("projects").doc(projectData.projectId).collection('tasks').doc(uuidv4()).set({ 
+                  taskName: taskName,
+                  taskDescription: taskDescription || null,
+                  taskDeadline: taskDeadline,
+                  assignedUser1: assignedUsers.user1 || null,
+                  assignedUser2: assignedUsers.user2 || null,
+                  assignedUser3: assignedUsers.user3 || null,
+                  assignedUser4: assignedUsers.user4 || null,
+                  assignedUser5: assignedUsers.user5 || null
+            })
+            .then(() => {
+                  console.log("Task successfully added!");
+            })
+            .catch((error) => {
+                  interaction.reply("An error occurred when attempting to save task to database. Try again or contact *jonw01*." + "\n <@" + interaction.user.id + ">");
+                  console.error("Error adding task: ", error);
+            });
+
+            // Build reply 
+            var reply = `:partying_face: **Successfully created task! (Project ${projectData.projectName})**`;
+            reply += "\n=========================================\n";
+            reply += `**Task Name**: \`${taskName}\` \n`;
+            if (taskDescription)
+                  reply += `**Task Description**: \`${taskDescription}\` \n`;
+            reply += "**Assigned Users**: ";
+            for (let i = 1; i <= 5; i++) {
+                  if (interaction.options.getUser(`assigned-user-${i}`))
+                        reply += `${interaction.options.getUser(`assigned-user-${i}`)}` + " ";
+            };
+            reply += "\n";
+            reply += `**Task Deadline**: \`${taskDeadline}\` \n`;
+            reply += "\n";
+
+            // Send reply to user
+            interaction.reply(`${reply} \n <@${interaction.user.id}>`);
+            return;
+      }
+
+      if (interaction.commandName === 'details') {
+            const projectName = interaction.options.getString('project-name');
+            const projectPassword = interaction.options.getString('project-password') || null;
+            
+            // Validate project name
+            const projectsRef = DB.collection('projects');
+            const snapshot = await projectsRef.where('projectName', '==', projectName).get();
+            if (snapshot.empty) {
+                  interaction.reply(`Project name '**${projectName}**' does not exist!\nCreate a **new project**, or find for a valid project name with */list-project-names*. \n <@${interaction.user.id}>`);
+                  return;
+            }
+
+            const projectData = snapshot.docs[0].data();
+
+            // Validate password
+            var tasksStr = "";
+            if (projectData.projectPassword) {
+                  if (!projectPassword) {
+                        interaction.reply(`Project \`${projectData.projectName}\` is protected with a password. Enter the correct password. \n <@${interaction.user.id}>`);
+                        return;
+                  } 
+                  const correctPassword = await passwordIsCorrect(projectPassword, projectData.projectPassword);
+                  
+                  if (!correctPassword) {
+                        interaction.reply(`Incorrect password for project \`${projectData.projectName}\`. Enter the __correct password__. \n <@${interaction.user.id}>`);
+                        return;
+                  }
+            }
+
+            tasksStr += `**Project**: \t\`${projectData.projectName}\`\n`;
+            if (projectData.projectDescription)
+                  tasksStr += `**Project Description**: \t\`${projectData.projectDescription}\`\n`;
+            tasksStr += `**Owned by**: <@${projectData.projectOwner}>\n\n`;
+            if (projectData.projectPassword) 
+                  tasksStr += `:lock: Protected by a password.\n`;
+            else 
+                  tasksStr += `:unlock: A public project\n`;
+
+            // List tasks
+            var projectRef = DB.collection('projects').doc(projectData.projectId);
+            const tasksRef = projectRef.collection('tasks');
+            const tasksSnapshot = await tasksRef.get();
+            const tasks = tasksSnapshot.docs.map((doc) => doc.data());
+
+            tasksStr += "==========================\n\n";
+
+            if (!tasks || tasks.empty) {
+                  tasksStr += "No tasks assigned in this project yet!\n";
+                  interaction.reply(`${tasksStr} \n <@${interaction.user.id}>`);
+                  return;
+            }
+
+            var tasksArray = tasks.map(task => ({
+                ...task,
+                taskDeadline: new Date(task.taskDeadline)  
+            }));
+
+            tasksArray.sort((a, b) => b.taskDeadline - a.taskDeadline);
+            
+            tasksArray = tasks.map(task => ({
+                  ...task,
+                  taskDeadline: task.taskDeadline.toString()
+            }));
+
+            tasksArray.forEach((task) => {
+                  tasksStr += `> **Task Name**: \`${task.taskName}\`\n`;
+                  if (task.taskDescription) 
+                        tasksStr += `> **Task Description**: \`${task.taskDescription}\`\n`;
+                  else 
+                        tasksStr += `> **Task Description**: \`-\`\n`;
+                  tasksStr += `> **Task Deadline**: \`${task.taskDeadline}\`\n`;
+                  if (task.assignedUser1)
+                        tasksStr += `> **Assigned User 1**: <@${task.assignedUser1}>\n`;
+                  else 
+                        tasksStr += `> **Assigned User 1**: \`-\`\n`;
+                  
+                  if (task.assignedUser2)
+                        tasksStr += `> **Assigned User 2**: <@${task.assignedUser2}>\n`;
+                  else 
+                        tasksStr += `> **Assigned User 2**: \`-\`\n`;
+                  
+                  if (task.assignedUser3)
+                        tasksStr += `> **Assigned User 3**: <@${task.assignedUser3}>\n`;
+                  else 
+                        tasksStr += `> **Assigned User 3**: \`-\`\n`;
+
+                  if (task.assignedUser4)
+                        tasksStr += `> **Assigned User 4**: <@${task.assignedUser4}>\n`;
+                  else 
+                        tasksStr += `> **Assigned User 4**: \`-\`\n`;
+
+                  if (task.assignedUser5)
+                        tasksStr += `> **Assigned User 5**: <@${task.assignedUser5}>\n`;
+                  else 
+                        tasksStr += `> **Assigned User 5**: \`-\`\n`;
+
+                  tasksStr += "\n\n";
+            });
+            interaction.reply(`${tasksStr} \n <@${interaction.user.id}>`);
+            return;
+      }
+
+      if (interaction.commandName === 'list-project-names') {
+            const projectsRef = DB.collection('projects');
+            const projectSnapshot = await projectsRef.get();
+            const projectData = projectSnapshot.docs.map(doc => doc.data());
+
+            var reply = "";
+            if (projectData.length > 0) {
+                  reply += "**List of Existing Project Names**\n";
+                  reply += "====================================\n";
+                  for (let i = 0; i < projectData.length; i++) {
+                        if (projectData[i].projectPassword) {
+                              reply += ":lock: " + projectData[i].projectName + "\n";
+                        }
+                        else {
+                              reply += ":unlock: " + projectData[i].projectName + "\n";
+                        }
+                  }
+            } else {
+                  reply += "No projects yet!\nCreate one with `/new-project`!\n";
+            }
+            interaction.reply(`${reply}\n <@${interaction.user.id}>`);
+            return;
+      }
+
+      if (interaction.commandName === 'delete-task') {
+            const projectName = interaction.options.getString('project-name');
+            const projectPassword = interaction.options.getString('project-password');
+            const taskName = interaction.options.getString('task-name');
+        
+            // Validate project name
+            const projectsRef = DB.collection('projects');
+            const snapshot = await projectsRef.where('projectName', '==', projectName).get();
+            if (snapshot.empty) {
+                interaction.reply(`Project name '**${projectName}**' does not exist!\nCreate a **new project**, or find a valid project name with */list-project-names*. \n <@${interaction.user.id}>`);
+                return;
+            }
+        
+            const projectData = snapshot.docs[0].data();
+        
+            // Validate project password if required
+            if (projectData.projectPassword) {
+                if (!projectPassword) {
+                    interaction.reply(`Project \`${projectData.projectName}\` is protected with a password. Enter the correct password. \n <@${interaction.user.id}>`);
+                    return;
+                } 
+                const correctPassword = await passwordIsCorrect(projectPassword, projectData.projectPassword);
+                
+                if (!correctPassword) {
+                    interaction.reply(`Incorrect password for project \`${projectData.projectName}\`. Enter the __correct password__. \n <@${interaction.user.id}>`);
+                    return;
+                }
+            }
+        
+            // Validate task name
+            const tasksRef = DB.collection('projects').doc(projectData.projectId).collection('tasks');
+            const tasksSnapshot = await tasksRef.where('taskName', '==', taskName).get();
+            if (tasksSnapshot.empty) {
+                interaction.reply(`Task '**${taskName}**' does not exist!\nCreate a **new task**, or enter the correct task name that you want to remove. \n <@${interaction.user.id}>`);
+                return;
+            }
+        
+            const taskId = tasksSnapshot.docs[0].id;
+        
+            // Delete the task
+            await DB.collection('projects').doc(projectData.projectId).collection('tasks').doc(taskId).delete();
+        
+            // Reply with a success message
+            interaction.reply(`Task '**${taskName}**' has been successfully deleted from project \`${projectData.projectName}\`. \n<@${interaction.user.id}>`);
+      }
+})
+
 function isValidDateTimeString(dateString) {
       // Regex pattern to match the format DD/MM/YYYY HH:MM:SS
       const regex = /^\d{2}\/\d{2}\/\d{4} \d{2}:\d{2}:\d{2}$/;
